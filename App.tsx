fd98abfc5f7f1e5d76ed331183f38a7dc3548471
Andis Surface <andreas.riepl@gmx.net>
2021-10-14 15:10:47 -0700

    finished app skeleton

--- App.tsx
+++ App.tsx
@@ -1,70 +1,57 @@
-import React, {useState, useEffect} from 'react';
-import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import React, {useState} from 'react';
 import {Provider as StoreProvider} from 'react-redux';
-import ReduxThunk from 'redux-thunk'
-import {createStore, applyMiddleware} from 'redux';
-import * as Font from 'expo-font';
-import {NavigationContainer} from '@react-navigation/native';
-import ErrorBoundary from 'react-native-error-boundary'
+import ReduxThunk from 'redux-thunk';
+import {applyMiddleware, createStore} from 'redux';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {composeWithDevTools} from 'redux-devtools-extension';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {NavigationContainer} from "@react-navigation/native";
+import ErrorBoundary from 'react-native-error-boundary'
 
-import {AppRegistry} from 'react-native';
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
-
-AppRegistry.registerComponent('miragon-construction-organizer', () => gestureHandlerRootHOC(App));
-
-
-import 'intl';
-import 'intl/locale-data/jsonp/de';
-import {rootReducer} from "./store/reducers/rootReducer"
-import ErrorFatal from "./components/ErrorFatal";
-
-import Colors from "./constants/Colors";
+import Colors from './constants/Colors';
 import MainNavigator from "./navigation/MainNavigator";
+import ErrorFatal from "./components/ErrorFatal";
+import {rootReducer} from "./store/reducers/rootReducer";
 
 
-let store;
-if (__DEV__) {
-  store =
-      createStore(
-          rootReducer,
-          composeWithDevTools(applyMiddleware(ReduxThunk))
-      )
-} else {
-  store = createStore(rootReducer, applyMiddleware(ReduxThunk))
+
+const getStore = () => {
+    if (__DEV__) {
+        return createStore(
+            rootReducer,
+            composeWithDevTools(applyMiddleware(ReduxThunk))
+        );
+    } else {
+        return createStore(rootReducer, applyMiddleware(ReduxThunk));
+    }
 }
 
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-};
 
 const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: Colors.primary,
-    accent: Colors.secondary
-  }
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: Colors.primary,
+        accent: Colors.secondary
+    }
 };
 
 export default function App() {
-  return (
 
-      <StoreProvider store={store}>
-        <PaperProvider theme={theme}>
-          <ErrorBoundary
-              FallbackComponent={({error, resetError}) =>
-                  <ErrorFatal error={error} resetError={resetError}/>
-              }>
-            <NavigationContainer>
-              <MainNavigator/>
-            </NavigationContainer>
-          </ErrorBoundary>
-        </PaperProvider>
-      </StoreProvider>
-
-  );
+    return (
+        <StoreProvider store={getStore()}>
+            <PaperProvider theme={theme}>
+                <SafeAreaProvider>
+                    <ErrorBoundary
+                        FallbackComponent={({error, resetError}) =>
+                            <ErrorFatal error={error} resetError={resetError}/>
+                        }>
+                        <NavigationContainer>
+                            <MainNavigator/>
+                        </NavigationContainer>
+                    </ErrorBoundary>
+                </SafeAreaProvider>
+            </PaperProvider>
+        </StoreProvider>
+    );
 }
---
+++ constants/Colors.tsx
@@ -0,0 +1,10 @@
+export default {
+    primary: 'rgb(80,140,200)',
+    secondary: 'rgb(200,150,0)',
+    inactive: 'rgb(220,220,220)',
+    cation: 'rgb(220, 0, 0)',
+
+    primaryBackground: 'rgba(80,140,200,0.2)',
+    secondaryBackground: 'rgba(230, 200, 130, 0.2)',
+    cautionBackground: 'rgba(220, 0, 0, 0.2)'
+};
